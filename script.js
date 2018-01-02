7a48892edb7a13e8abdb7e8ba147d7187ac93e6e
“silverwind85 <slcieslar@gmail.com>
2018-01-02 19:07:08 +0100

    Frontend

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://dashboard.heroku.com/apps/mysterious-tor-53135/activity/v1/task/';
+  var apiRoot = 'https://mysterious-tor-53135.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 	//$('.test').html(datatableRowTemplate);
